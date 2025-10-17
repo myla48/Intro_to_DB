74424b0edeb3f39cacb01f15887b5abfc80a2316
myla48 <ruhamazerihun2@gmail.com>
2025-10-17 14:57:48 +0300

    Create alx_book_store.sql

---
+++ alx_book_store.sql
@@ -0,0 +1,45 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+-- Table: Authors
+CREATE TABLE authors (
+  author_id INT PRIMARY KEY,
+  author_name VARCHAR(215)
+);
+
+-- Table: Books
+CREATE TABLE books (
+  book_id INT PRIMARY KEY,
+  title VARCHAR(130),
+  author_id INT,
+  price DOUBLE,
+  publication_date DATE,
+  FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+-- Table: Customers
+CREATE TABLE customers (
+  customer_id INT PRIMARY KEY,
+  customer_name VARCHAR(215),
+  email VARCHAR(215),
+  address TEXT
+);
+
+-- Table: Orders
+CREATE TABLE orders (
+  order_id INT PRIMARY KEY,
+  customer_id INT,
+  order_date DATE,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- Table: Order_Details
+CREATE TABLE order_details (
+  order_detail_id INT PRIMARY KEY,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE,
+  FOREIGN KEY (order_id) REFERENCES orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
